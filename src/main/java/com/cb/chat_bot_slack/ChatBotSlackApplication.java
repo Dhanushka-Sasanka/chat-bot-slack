9ebf6cf9d84fd482c9bcb68b5031a9e2761200bf
Dhanushka-Sasanka <dhanushkasasankapayza@gmail.com>
2023-05-23 10:00:41 +0530

    first commit

---
+++ src/main/java/com/cb/chat_bot_slack/ChatBotSlackApplication.java
@@ -0,0 +1,13 @@
+package com.cb.chat_bot_slack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotSlackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBotSlackApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cb/chat_bot_slack/controller/CommonViewController.java
@@ -0,0 +1,22 @@
+package com.cb.chat_bot_slack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@CrossOrigin("http://localhost:8080")
+public class CommonViewController {
+    /**
+     * this method for render the upload APK file view
+     * @return index.html
+     */
+    @RequestMapping("/index")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
+    }
+
+}
